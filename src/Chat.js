ed9e2eacc0dd00ae524ab6e23eaee8443818f4be
Aman Singh <aamansingh5096@gmail.com>
2023-01-25 10:13:28 +0530

    Structuring the app

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chat() {
+  return (
+    <div>
+      <h1>I am chat component</h1>
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBe6VWeIdEoTUY9k-ud4YPPZ0ZHGcS26Vc",
+    authDomain: "whatsapp-kdas.firebaseapp.com",
+    projectId: "whatsapp-kdas",
+    storageBucket: "whatsapp-kdas.appspot.com",
+    messagingSenderId: "211901526746",
+    appId: "1:211901526746:web:23bb6e5e84bc262b2a6be3"
+  };
